edcaf5e29485bdd5d5dc80cfe15b758f6726e32f
prakhar-shukla17 <prakhars210.26@gmail.com>
2025-04-24 21:28:51 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="energy_consumption_prediction",
+    version="0.0.0",
+    author="Prakhar",
+    author_email="Prakhars210.26@gmail.com",
+    packages=find_packages()
+)
